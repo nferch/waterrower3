84addcbd8c6b804bd1c7eb357d5325727bed4ba8
Nathan A. Ferch <nf@marginal.net>
2016-01-11 19:50:11 -0500

    add a formatter for google sheets

--- waterrower3/util.py
+++ waterrower3/util.py
@@ -4,8 +4,15 @@ import datetime
 def format_sec(time_sec):
     return("{0:02}:{1:02}".format(time_sec / 60, time_sec % 60))
 
+
+def format_time(time_sec):
+    return("{0:02}:{1:02}:{2:02}".format(
+        time_sec / 3600, time_sec / 60, time_sec % 60))
+
+
 def format_timedelta(timestamp):
     return("{}s ago".format((datetime.datetime.now() - timestamp).seconds))
+
 
 def format_longest(longest):
     if longest:
