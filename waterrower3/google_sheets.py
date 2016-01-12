48bedc3ef74f2b9f25af1af5c64c09abaa451330
Nathan A. Ferch <nf@marginal.net>
2016-01-11 19:29:07 -0500

    make sheets updater more flexible
    will now dynamically find rows for data based on header
    factor out connection to make unit testing easier

--- waterrower3/google_sheets.py
+++ waterrower3/google_sheets.py
@@ -2,39 +2,63 @@ import json
 
 
 import gspread
-from oauth2client.client import SignedJwtAssertionCredentials
+import oauth2client.client as oauth2_client
 
 
-class GoogleSheetsUpdater(object):
-    DEFAULT_COLUMN = "Date"
-    DATA_POSITIONS = {"Date": 2,
-                      "Distance": 4,
-                      "Time": 5}
-
-    def __init__(self, creds_file, sheet):
+class GoogleSheetsConnector(object):
+    def __init__(self, creds_file):
         json_key = json.load(open(creds_file))
         scope = ['https://spreadsheets.google.com/feeds']
 
-        creds = SignedJwtAssertionCredentials(json_key['client_email'],
-                                              json_key['private_key'].encode(),
-                                              scope)
+        creds = oauth2_client.SignedJwtAssertionCredentials(
+            json_key['client_email'],
+            json_key['private_key'].encode(),
+            scope)
 
         self.gc = gspread.authorize(creds)
-        self.wks = self.gc.open(sheet).sheet1
+
+
+class GoogleSheetsUpdater(object):
+    MAX_HEADER_ROW_SEARCH = 5
+
+    def __init__(self, gc, sheet):
+        self.gc = gc
+        self.wks = self.gc.gc.open(sheet).sheet1
 
     def update(self, update_data):
-        last_row = self.find_last_row() + 1
+        header_cols = self.find_column_headers(update_data.keys())
+        last_row = self.find_last_row(header_cols.iteritems().next()[1]) + 1
 
         if last_row > self.wks.row_count:
             self.wks.add_rows(10)
             return(self.update(update_data))
 
         for key in update_data:
-            self.wks.update_cell(last_row, self.DATA_POSITIONS[key],
+            self.wks.update_cell(last_row, header_cols[key],
                                  update_data[key])
 
-    def find_last_row(self):
-        datecol = self.wks.col_values(self.DATA_POSITIONS[self.DEFAULT_COLUMN])
+    def find_last_row(self, column):
+        """last non-empty row for given column starting from bottom of sheet."""
+        col_vals = self.wks.col_values(column)
         return(
-            next(i for i, j in reversed(list(enumerate(datecol)))
+            next(i for i, j in reversed(list(enumerate(col_vals)))
                  if j != '') + 1)
+
+    def find_column_headers(self, headers):
+        """Find column ids based on a header row.
+
+        :param headers: columns to find
+        :type headers: list
+        :returns: dict of headers/column ids
+        """
+        for j in range(1, self.MAX_HEADER_ROW_SEARCH+1):
+            try:
+                row = self.wks.row_values(j)
+            except gspread.exceptions.HTTPError as e:
+                if e.code == 400:
+                    raise LookupError("Can't find header row")
+                else:
+                    raise(e)
+            if set(headers) < set(row):
+                return({h: row.index(h)+1 for h in headers})
+        raise LookupError("Can't find header row")
